2c9348af0820225be81f5f8302822492be648b32
Region3 <126637834+Jimenth@users.noreply.github.com>
2025-12-15 16:50:53 -0500

    Update WarTycoon.lua

--- WarTycoon.lua
+++ WarTycoon.lua
@@ -1,6 +1,6 @@
---! optimization 2
-loadstring(game: HttpGet("https://raw.githubusercontent.com/Sploiter13/severefuncs/refs/heads/main/merge.lua"))();
-local Load = luau.load(game: HttpGet("https://raw.githubusercontent.com/DCHARLESAKAMRGREEN/Severe-Luas/main/Libraries/Pseudosynonym.lua"))
+--! optimize 2
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Sploiter13/severefuncs/refs/heads/main/merge.lua"))();
+local Load = luau.load(game:HttpGet("https://raw.githubusercontent.com/DCHARLESAKAMRGREEN/Severe-Luas/main/Libraries/Pseudosynonym.lua"))
 local Version = "1.0.0"
 
 local Offsets = {
@@ -8,56 +8,53 @@ local Offsets = {
 }
 
 local Workspace = game:GetService("Workspace")
-local Tycoons = Workspace.  Tycoon.  Tycoons
+local Tycoons = Workspace.Tycoon.Tycoons
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
 local Library = Load()
-local Window = Library:  CreateWindow({
-    Title = "War Tycoon | " .. Version,
+local Window = Library:CreateWindow({
+    Title = "War Tycoon | "..Version,
     Tag = LocalPlayer.DisplayName,
     Keybind = "Alt",
     AutoShow = true
 })
 
--- // Assistants Tab \\ --
-local AssistantsTab = Window:AddTab({ Name = "Assistants" })
-local AssistantsContainer = AssistantsTab:AddContainer({ Name = "Assistants", Side = "Left", AutoSize = true })
-local MainAssistants = AssistantsContainer:AddTab("Main")
-local CollectCash = MainAssistants:  AddToggle({ Name = "Loop Collect Cash", Value = false, Tooltip = "Automatically Collect Cash" })
-local CompleteTycoon = MainAssistants:  AddToggle({ Name = "Loop Complete Tycoon", Value = false, Tooltip = "Automatically Complete Tycoon" })
+--//Assistants Tab\\--
+local AssistantsTab = Window:AddTab({Name = "Assistants"})
+local Main = AssistantsTab:AddContainer({Name = "Main",Side = "Left",AutoSize = true})
+local Automation = Main:AddTab("Automation")
+local AutomateCollection = Automation:AddToggle({Name = "Automate Collection",Value = false,Tooltip = "Automatically Collect Cash"})
+local AutomateCompletion = Automation:AddToggle({Name = "Automate Completion",Value = false,Tooltip = "Automatically Complete Tycoon"})
 
--- // Settings Tab \\ --
-local SettingsTab = Window:  AddTab({ Name = "Settings" })
-local MenuContainer = SettingsTab:AddContainer({ Name = "Menu", Side = "Left", AutoSize = true })
+--//Settings Tab\\--
+local SettingsTab = Window:AddTab({Name = "Settings"})
+local MenuContainer = SettingsTab:AddContainer({Name = "Menu",Side = "Left",AutoSize = true})
 MenuContainer:AddMenuBind({})
 MenuContainer:AddSeparator()
-MenuContainer:AddWatermark({ Watermark = "Hello Croski", ShowFPS = true })
+MenuContainer:AddWatermark({Watermark = "Hello Croski",ShowFPS = true})
 MenuContainer:AddKeybindList({})
-MenuContainer:AddButton({ Name = "Unload", Unsafe = true, Callback = function() Library:Unload() end })
+MenuContainer:AddButton({Name = "Unload",Unsafe = true,Callback = function()Library:Unload()end})
 
 local function GetCompletion()
     local PlayerGui = LocalPlayer:FindFirstChild("PlayerGui")
     if not PlayerGui then return 0 end
 
     local HUD = PlayerGui.UI.Container.HUD.Menu.HUD
-
     local Left = HUD:FindFirstChild("Left")
     if not Left then return 0 end
 
     local Completion = Left:FindFirstChild("TycoonCompletion")
     if not Completion then return 0 end
-    
+
     local Bar = Completion:FindFirstChild("Bar")
     if not Bar then return end
 
     local Progress = Bar:FindFirstChild("BarProgressAmount")
     if not Progress then return end
 
-    local RealAmount = 0
-    local Percentage = tostring(memory.readstring(Progress, Offsets.TextLabelText))
-    if Progress then RealAmount = tonumber(Percentage:match("(%d+%.?%d*)")) end
-
+    local Percentage = tostring(memory.readstring(Progress,Offsets.TextLabelText))
+    local RealAmount = tonumber(Percentage:match("(%d+%.?%d*)"))
     return RealAmount or 0
 end
 
@@ -82,17 +79,15 @@ local function GetRebirths()
 end
 
 local function GetPlot()
-    for Index, Plot in ipairs(Tycoons:GetChildren()) do
-        local Owner = Plot: GetAttribute("Owner")
-        if Owner and Owner == LocalPlayer. Name then
+    for _,Plot in ipairs(Tycoons:GetChildren()) do
+        if Plot:GetAttribute("Owner") == LocalPlayer.Name then
             return Plot
         end
     end
-    return nil
 end
 
-local function SetPosition(Object, Target)
-    Object.Position = Target + vector.create(0.2, 0.2, 0.2)
+local function SetPosition(Object,Target)
+    Object.Position = Target + vector.create(0.2,0.2,0.2)
 end
 
 local function AutoCollect()
@@ -106,15 +101,16 @@ local function AutoCollect()
     if not Plot then return end
 
     local Collector = Plot:FindFirstChild("Essentials") and Plot.Essentials:FindFirstChild("CollectorParts")
-
     local Object = Collector and Collector:FindFirstChild("Display")
     if Object then
-        SetPosition(HRP, Object.Position + vector.create(0, 0, -3))
+        SetPosition(HRP,Object.Position)
     end
 end
 
+Automation:AddButton({Name = "Collect Cash",Unsafe = false,Callback = function()AutoCollect()end})
+
 local function AutoComplete()
-    if not CompleteTycoon.Value then return end
+    if not AutomateCompletion.Value then return end
 
     local Character = LocalPlayer.Character
     if not Character then return end
@@ -128,80 +124,62 @@ local function AutoComplete()
     local Items = Plot:FindFirstChild("UnpurchasedButtons")
     if not Items then return end
 
-    local RebirthButtons = {}
-    local OilButtons = {}
-    local CashButtons = {}
+    local RebirthButtons,OilButtons,CashButtons = {},{},{}
 
-    for _, Item in ipairs(Items:GetChildren()) do
+    for _,Item in ipairs(Items:GetChildren()) do
         local ButtonType = Item:GetAttribute("ButtonType")
         local name = Item.Name or ""
+        local Primary = Item:FindFirstChild("Gradient")
+
         if ButtonType == "Rebirth" then
             local Requirement = tonumber(Item:GetAttribute("RebirthRequirement"))
-            local Primary = Item:FindFirstChild("Gradient")
-            if Requirement and Primary then
-                if GetRebirths() >= Requirement then
-                    RebirthButtons[#RebirthButtons + 1] = {
-                        Pad = Primary,
-                        Req = Requirement
-                    }
-                end
+            if Requirement and Primary and GetRebirths() >= Requirement then
+                RebirthButtons[#RebirthButtons+1] = {Pad = Primary,Req = Requirement}
             end
         elseif ButtonType == "Cash" then
             local Price = tonumber(Item:GetAttribute("Price"))
-            local Primary = Item:FindFirstChild("Gradient")
             if Price and Primary then
-                if name:sub(1, 3) == "Oil" then
-                    OilButtons[#OilButtons + 1] = {
-                        Pad = Primary,
-                        Price = Price
-                    }
+                if name:sub(1,3) == "Oil" then
+                    OilButtons[#OilButtons+1] = {Pad = Primary,Price = Price}
                 else
-                    CashButtons[#CashButtons + 1] = {
-                        Pad = Primary,
-                        Price = Price
-                    }
+                    CashButtons[#CashButtons+1] = {Pad = Primary,Price = Price}
                 end
             end
         end
     end
 
     if #RebirthButtons > 0 then
-        table.sort(RebirthButtons, function(a, b) return a.Req < b.Req end)
-        local Target = RebirthButtons[1]
-        SetPosition(HRP, Target.Pad.Position)
+        table.sort(RebirthButtons,function(a,b)return a.Req < b.Req end)
+        SetPosition(HRP,RebirthButtons[1].Pad.Position)
         return
     end
 
     if #OilButtons > 0 then
-        table.sort(OilButtons, function(a, b) return a.Price < b.Price end)
+        table.sort(OilButtons,function(a,b)return a.Price < b.Price end)
         local Target = OilButtons[1]
         if GetCash() < Target.Price then
             AutoCollect()
             return
         end
-        SetPosition(HRP, Target.Pad.Position)
+        SetPosition(HRP,Target.Pad.Position)
         return
     end
 
-    table.sort(CashButtons, function(a, b)
-        return a.Price < b.Price
-    end)
-
     if #CashButtons == 0 then return end
+    table.sort(CashButtons,function(a,b)return a.Price < b.Price end)
 
     local CheapestButton = CashButtons[1]
-
     if GetCash() < CheapestButton.Price then
         AutoCollect()
         return
     end
 
-    SetPosition(HRP, CheapestButton.Pad.Position)
+    SetPosition(HRP,CheapestButton.Pad.Position)
 end
 
 task.spawn(function()
     while true do
-        if CollectCash. Value then
+        if AutomateCollection.Value then
             AutoCollect()
         end
         task.wait(0.4)
@@ -210,7 +188,7 @@ end)
 
 task.spawn(function()
     while true do
-        if CompleteTycoon. Value then
+        if AutomateCompletion.Value then
             AutoComplete()
             task.wait(0.5)
         end
